5d74e82b8f78a3eec99fb76452bcdc9aa97a31f2
Belloshehu <belloshehu1@gmail.com>
2022-09-01 15:41:21 +0100

    Add feature to remove item on win

--- game/index.js
+++ game/index.js
@@ -1,12 +1,12 @@
 const pointer = document.querySelector('#pointer')
 const playBtn = document.querySelector('button#play')
-let transformValue = 2000
+// let transformValue = 2000
 let itemHeight = 80; // image-wrapper + gaps
 
 const h1 = document.querySelector('article.image-wrapper h1')
 
 // image wrapper items
-const imageWrappers = document.querySelectorAll('.image-wrapper')
+let imageWrappers = document.querySelectorAll('.image-wrapper')
 
 // score h1
 const scoreH1 = document.querySelector('aside #score')
@@ -18,15 +18,28 @@ const displayH2 = document.querySelectorAll('aside #display-wrapper h2')
 
 // modal
 const modal = document.querySelector('div.modal')
+
+// itmes wrapper
+const itemsWrapper = document.querySelector('div#items')
+
+// get height of the item wrapper:
+let  height = itemsWrapper.getBoundingClientRect().height
+let imageWrapperHeight = (height / imageWrappers.length);
+
 //score
 let score = 0
 let maxTrial = 3
 let trial = 0
+let numberOfItems = imageWrappers.length;
 
 function translateY(count = 1){
-    pointer.style.transform = `translateY(${itemHeight * 8}px)`
+    // move pointer to the bottom
+    pointer.style.transform = `translateY(${height - 50}px)`
+    // transform: translateY(height);
     setTimeout(()=>{
-        pointer.style.transform = `translateY(${itemHeight * (count)}px)`
+        pointer.style.transform = `translateY(${
+            (imageWrapperHeight * count) - ((imageWrapperHeight + 25) / 2)
+        }px)`
     }, 1000)
 }
 
@@ -39,23 +52,45 @@ function playGame(e){
     }, 1500)
     let timer2 = setTimeout(()=>{
         clearTimeout(timer1)
-        let count = Math.floor((Math.random()*8)+1)
+        let count = Math.floor((Math.random() * numberOfItems)+1)
         translateY(count)
         let selection = selectionInput.value
         displayH2[1].innerHTML = `Pointer stopped at ${count}`
-        console.log(count, selection)
+        
         if(count === parseInt(selection)){
             score = score + 1
             scoreH1.innerHTML = score
-            console.log('score', score)
+            trial = trial + 1
             
             // change bakground color to grey and back to white
-            imageWrappers[count - 1].style.backgroundColor = 'grey'
+            imageWrappers[count - 1].style.backgroundColor = 'green'
             setTimeout(()=>{
                 imageWrappers[count - 1].style.backgroundColor = 'white'
+
+                // remove the highlighted item
+                itemsWrapper.removeChild(imageWrappers[count - 1])
+                const  numberH1s = document.querySelectorAll('.image-wrapper h1');
+                numberH1s.forEach((numberH1, index) => {
+                    numberH1.innerHTML = index + 1;
+                });
+                console.log(numberH1s)
+
+                // update the imageWrappers variable
+                imageWrappers = document.querySelectorAll('.image-wrapper')
+                // get the current number of imageWrapper items
+                numberOfItems = imageWrappers.length;
+
+                // update the height of the items Wrapper
+                height = itemsWrapper.getBoundingClientRect().height
+                imageWrapperHeight = (height / imageWrappers.length);
+
+
             }, 1000)
+
+
         }else{
             score = score - 1
+            trial = trial - 1
             scoreH1.innerHTML = score
 
             // change bakground color to red and back to white
